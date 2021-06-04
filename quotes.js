739e1d6e1527471b18d2bc148f10a199638aa3c2
Charles Commas <ch4rles.commas@gmail.com>
2021-06-05 01:31:23 +0700

    second commit

---
+++ quotes.js
@@ -0,0 +1,801 @@
+let quotes = [
+    
+	{
+		number: 1,
+		text: "If the young only knew, if the old only could.",
+		quotee: "French proverbs",
+	},
+
+	{
+		number: 2,
+		text: "Memory is the scribe of the soul.",
+		quotee: "Max vandenberg (The Book Thief)",
+	},
+
+	{
+		number: 3,
+		text: "Talent is something you make bloom, instinct is something you polish.",
+		quotee: "Oikawa (Haikyu!)",
+	},
+
+	{
+		number: 4,
+		text: "People talk loud when they wanna act smart.",
+		quotee: "Squidward (Spongebob Squarepants)",
+	},
+
+	{
+		number: 5,
+		text: "A day without laughter, is a day wasted.",
+		quotee: "Charlie Chaplin",
+	},
+
+	{
+		number: 6,
+		text: "The more you try to forget, the more you'll remember.",
+		quotee: " Father Muliadi",
+	},
+
+	{
+		number: 7,
+		text: "There is a difference between you and me. We both looked into the abyss, but when it looked back at us, you blinked.",
+		quotee: "Batman (Justice League)",
+	},
+
+	{
+		number: 8,
+		text: "Bebek berjalan berbondong-bondong, akan tetapi burung elang terbang sendirian. (Ducks walk in droves, but eagles fly alone.)",
+		quotee: "Ir. Soekarno",
+	},
+
+	{
+		number: 9,
+		text: "When crows flock, they might even kill a huge, white eagle.",
+		quotee: "Oikawa (Haikyu!)",
+	},
+
+	{
+		number: 10,
+		text: "In their last moments, people show you who they really are.",
+		quotee: "Joker (Dark Knight)",
+	},
+
+	{
+		number: 11,
+		text: "Sometimes we do wrong things for the right reasons.",
+		quotee: "Kungfu Panda 3",
+	},
+
+	{
+		number: 12,
+		text: "Any action will cause a reaction. You better be careful.",
+		quotee: "Uchiha Obito",
+	},
+
+	{
+		number: 13,
+		text: "Fail. I don't do fail.",
+		quotee: "Kick Buttowski",
+	},
+
+	{
+		number: 14,
+		text: "A hero can be anyone. Even a man doing something as simple and reassuring as putting a coat around a young boy's shoulders to let him know that the world hadn't ended.",
+		quotee: "Batman (The Dark Knight Rises)",
+	},
+
+	{
+		number: 15,
+		text: "Robots, nothing here (pointing to the heart), just lights and clockwork.",
+		quotee: "I, Robot",
+	},
+
+	{
+		number: 16,
+		text: "You can't tell someone they love you.",
+		quotee: "Dom (Furious 7)",
+	},
+
+	{
+		number: 17,
+		text: "When life robs you, sometimes you have to rob it back.",
+		quotee: "The Book Thief",
+	},
+
+	{
+		number: 18,
+		text: "Cheaters never win, and winners never cheat.",
+		quotee: "Fairly Odd Parents",
+	},
+
+	{
+		number: 19,
+		text: "With nothing to protect, you will be strong.",
+		quotee: "Attack On Titans",
+	},
+
+	{
+		number: 20,
+		text: "All it takes is one bad day to reduce the sanest man alive to lunacy.. just one bad day.",
+		quotee: "Joker",
+	},
+
+	{
+		number: 21,
+		text: "We are always stronger, when we're doing the right thing.",
+		quotee: "Rose Wilson (Justice League)",
+	},
+
+	{
+		number: 22,
+		text: "We are the generation of idiots. Smart phones, dumb people.",
+		quotee: "Look up",
+	},
+
+	{
+		number: 23,
+		text: "Life is like riding a bicycle, to keep your balance, you must keep moving.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 24,
+		text: "First they ignore you, then they laugh at you, then they fight you, then you win.",
+		quotee: "Mahatma Gandhi",
+	},
+
+	{
+		number: 25,
+		text: "Keep smiling, and one day life will get tired of upsetting you.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 26,
+		text: "If you can't explain it simply, you don't understand it well enough.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 27,
+		text: "The difference between stupidity and genius, is that genius has it's limits.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 28,
+		text: "A tree without root, is a piece of wood.",
+		quotee: "Marco P. White",
+	},
+
+	{
+		number: 29,
+		text: "And this part of my life, this little part, is called happyness.",
+		quotee: "Pursuit of happyness",
+	},
+
+	{
+		number: 30,
+		text: "Ohh, and try to have fun. Otherwise, what's the point?",
+		quotee: "Kick Ass 2",
+	},
+
+	{
+		number: 31,
+		text: "What you do right now, makes difference.",
+		quotee: "Black Hawk Down",
+	},
+
+	{
+		number: 32,
+		text: "If your eyes could speak, what would they say?",
+		quotee: "The Book Thief",
+	},
+
+	{
+		number: 33,
+		text: "A clever person solves a problem, a wise person avoids it.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 34,
+		text: "Imagination is more important than knowledge, for knowledge is limited, whereas imagination embraces the entire world.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 35,
+		text: "If you can't beat them, join them.",
+		quotee: "The hundred foot journey",
+	},
+
+	{
+		number: 36,
+		text: "A miracle is what seems impossible, but happens anyway.",
+		quotee: "Men In Black 3",
+	},
+
+	{
+		number: 37,
+		text: "The only impossible thing is the impossibility itself.",
+		quotee: "Spongebob Squarepants",
+	},
+
+	{
+		number: 38,
+		text: "If you're good at something, never do it for free.",
+		quotee: "Joker",
+	},
+
+	{
+		number: 39,
+		text: "Sometimes people are beautiful, not in looks, not in what they say, just in what they are.",
+		quotee: "The Book Thief",
+	},
+
+	{
+		number: 40,
+		text: "One must be a little crazy to write a good novel.",
+		quotee: "John Gardner",
+	},
+
+	{
+		number: 41,
+		text: "With great power, comes great responsibility.",
+		quotee: "Spiderman",
+	},
+
+	{
+		number: 42,
+		text: "You can't blame gravity for falling in love.",
+		quotee: "Albert Einstein",
+	},
+
+	{
+		number: 43,
+		text: "Anything's possible if you've got enough nerve.",
+		quotee: "J. K. Rowling",
+	},
+
+	{
+		number: 44,
+		text: "I met so many young men over the years who have thought they were running at their enemy, when the truth was, they were running to me.",
+		quotee: "Death (The Book Thief)",
+	},
+
+	{
+		number: 45,
+		text: "The moment you underestimate your enemy, you have already lost.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 46,
+		text: "Don't let them know that they got to you.",
+		quotee: "Nick Wilde (Zootopia)",
+	},
+
+	{
+		number: 47,
+		text: "But playing in a match has gotta be fun, right?",
+		quotee: "Koganei Shinji (Kuroko no Basuke)",
+	},
+
+	{
+		number: 48,
+		text: "Time is against you, and you're being careless with it.",
+		quotee: "Alice Through The Looking Glass",
+	},
+
+	{
+		number: 49,
+		text: "Isn't what drives most people childish in general?",
+		quotee: "Tendou Satori (Haikyuu!!)",
+	},
+
+	{
+		number: 50,
+		text: "A moment of neglect, a lifetime of regret.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 51,
+		text: "Give a man a mask, and he'll become his true self.",
+		quotee: "Joker",
+	},
+
+	{
+		number: 52,
+		text: "It's only after we've lost everything that we're free to do anything.",
+		quotee: "Fight Club",
+	},
+
+	{
+		number: 53,
+		text: "Life's simple. You make choices and you don't look back.",
+		quotee: "Han (The Fast And The Furious)",
+	},
+
+	{
+		number: 54,
+		text: "I walk slowly, but i never walk backward.",
+		quotee: "Abraham Lincoln",
+	},
+
+	{
+		number: 55,
+		text: "Cause sometimes when you lose your way, it's really just as well.. because you find yourself.",
+		quotee: "Find Yourself (Brad Paisley)",
+	},
+
+	{
+		number: 56,
+		text: "What we think, we become.",
+		quotee: "The Iron Lady",
+	},
+
+	{
+		number: 57,
+		text: "I'm not crazy. My reality is just different than yours.",
+		quotee: "Cheshire Cat (Alice In The Wonderland)",
+	},
+
+	{
+		number: 58,
+		text: "Sometimes people deserve more. Sometimes people deserve to have their faith rewarded.",
+		quotee: "Batman (Dark Knight)",
+	},
+
+	{
+		number: 59,
+		text: "Humans don't have wings, so they look for ways to.",
+		quotee: "Ukai Keishin (Haikyu!)",
+	},
+
+	{
+		number: 60,
+		text: "See, madness, as you know is like gravity. All it takes is a little push.",
+		quotee: "Joker (The Dark Knight)",
+	},
+
+	{
+		number: 61,
+		text: "Nothing comes that you can't handle, so on you go.",
+		quotee: "Our Town (OST. Cars)",
+	},
+
+	{
+		number: 62,
+		text: "We don't get to choose our time.",
+		quotee: "Ancient One (Doctor Strange)",
+	},
+
+	{
+		number: 63,
+		text: "Do not take life too seriously, you will never get out of it alive.",
+		quotee: "Elbert Hubbart",
+	},
+
+	{
+		number: 64,
+		text: "War is what happens when language fails.",
+		quotee: "Margaret Atwood",
+	},
+
+	{
+		number: 65,
+		text: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+		quotee: "Dr. Seuss",
+	},
+
+	{
+		number: 66,
+		text: "Nearly all men can stand adversity, but if you want to test a man's character, give him power.",
+		quotee: "Abraham Lincoln",
+	},
+
+	{
+		number: 67,
+		text: "Take away you can, give nothing back.",
+		quotee: "Capt. Jack Sparrow (POTC 1)",
+	},
+
+	{
+		number: 68,
+		text: "You either die as a hero, or you live long enough to see yourself become the villain.",
+		quotee: "Harvey Dent (Dark Knight)",
+	},
+
+	{
+		number: 69,
+		text: "The only normal people are the ones you don't know very well.",
+		quotee: "Alfred Adler",
+	},
+
+	{
+		number: 70,
+		text: "Hardwork may not always result in success, but it will never result in regret.",
+		quotee: "Moneyball",
+	},
+
+	{
+		number: 71,
+		text: "Everyone's a coward about something.",
+		quotee: "The Hurt Locker",
+	},
+
+	{
+		number: 72,
+		text: "Not because there are no obstacles, then you become optimistic. But because you are optimistic, then the obstacle is not felt.",
+		quotee: "Merry Riana",
+	},
+
+	{
+		number: 73,
+		text: "Two things defines you: your patience when you have nothing, and your attitude when you have everything.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 74,
+		text: "What you need isn't logic, it's trust.",
+		quotee: "Kise (KnB)",
+	},
+
+	{
+		number: 75,
+		text: "Often what is done is more important than what will be obtained. ",
+		quotee: "Arthur Ashe ",
+	},
+
+	{
+		number: 76,
+		text: "It's not the card you've been dealt, its how you play your hand.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 77,
+		text: "Tranquility is the door to truth",
+		quotee: "Oma's diary",
+	},
+
+	{
+		number: 78,
+		text: "To fight with another man, means war. But to struggle with yourself brings peace.",
+		quotee: "Oma's diary",
+	},
+
+	{
+		number: 79,
+		text: "Smile awhile, and life is worth while.",
+		quotee: "Oma's diary",
+	},
+
+	{
+		number: 80,
+		text: "The fact that he's different from other people, will probably end up being his strength.",
+		quotee: "Ushijima's father (Haikyuu!!)",
+	},
+
+	{
+		number: 81,
+		text: "Everyone parts with everything eventually, my dear.",
+		quotee: "Time - Alice Through The Looking Glass",
+	},
+
+	{
+		number: 82,
+		text: "Be careful, the darkness is always right there beside you.",
+		quotee: "Mayu (Noragami)",
+	},
+
+	{
+		number: 83,
+		text: "A boy with nothing, who changed everything.",
+		quotee: "Pelé",
+	},
+
+	{
+		number: 84,
+		text: " Because humans are complicated beasts, the monster said. How can a queen be both a good witch and a bad witch? How can a prince be a murderer and a saviour? How can an apothecary be evil-tempered but right-thinking? How can a parson be wrong-thinking but good-hearted? How can invisible men make themselves more lonely by being seen?",
+		quotee: "A Monster Calls",
+	},
+
+	{
+		number: 85,
+		text: "Sabbe satta bhavantu sukhitatta. (May all beings be well.)",
+		quotee: "Buddha Phrase",
+	},
+
+	{
+		number: 86,
+		text: "The person who says it cannot be done should not interrupt the person doing it.",
+		quotee: "Chinese Proverbs",
+	},
+
+	{
+		number: 87,
+		text: "There are two rules for success in life. One; never reveal everything you know.",
+		quotee: "The Riddler",
+	},
+
+	{
+		number: 88,
+		text: "Work for a cause, not for applause. Live life to express, not to impress. Don't strive to make your presence noticed, just make your absence felt.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 89,
+		text: "(Listening to What A Wonderful World song) I love that song. And it could be a wonderful world, if we stop doing bad shit to it.",
+		quotee: "Gibbons (xXx:RoXC)",
+	},
+
+	{
+		number: 90,
+		text: "I don't worry. Worrying doesn't agree things.",
+		quotee: "Tiny Tim (Living In The Sunlight)",
+	},
+
+	{
+		number: 91,
+		text: "Doubt kills more dreams than failure.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 92,
+		text: "One must keep on aiming higher.",
+		quotee: "Neji (Naruto Ninja Storm?)",
+	},
+
+	{
+		number: 93,
+		text: "People who are unable to abandon anything, will fail to bring about any change.",
+		quotee: "Armin (Shingeki no Kyojin)",
+	},
+
+	{
+		number: 94,
+		text: "Is getting rid of all the thorns in someone's path really what's best for them ?",
+		quotee: "Yoake (ReLife)",
+	},
+
+	{
+		number: 95,
+		text: "Talking through things only works when both sides want to.",
+		quotee: "Blitz Talker (Re: Creations)",
+	},
+
+	{
+		number: 96,
+		text: "Many a mickle makes a muckle.",
+		quotee: "Proverbs",
+	},
+
+	{
+		number: 97,
+		text: "Crocodiles are easy. They try to kill and eat you. People are harder. Sometimes they pretend to be your friend first.",
+		quotee: "Steve Irwin",
+	},
+
+	{
+		number: 98,
+		text: "It's always funny after you almost die.",
+		quotee: "Walterrific",
+	},
+
+	{
+		number: 99,
+		text: "You know, who you choose to be around you lets you know who you are.",
+		quotee: "Han (Fast And Furious)",
+	},
+
+	{
+		number: 100,
+		text: "No one know what's really going on in another person's life, and you never know how what you do will affect someone else.",
+		quotee: "13 Reasons Why",
+	},
+
+	{
+		number: 101,
+		text: "A bit of madness is key to give us new colors to see.",
+		quotee: "La La Land",
+	},
+
+	{
+		number: 102,
+		text: "It's so unfair. - Apparently the world is not a wish granting factory.",
+		quotee: "The Fault In Our Star",
+	},
+
+	{
+		number: 103,
+		text: "The only thing more terrifying than blindness is being the only one who can see.",
+		quotee: "Jose Saramago",
+	},
+
+	{
+		number: 104,
+		text: "You're the bad guy in somebody's story.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 105,
+		text: "If you want success, the foremost thing is to ensure that you are not the obstacle to it.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 106,
+		text: "Don't stop when you are tired, stop when you are done.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 107,
+		text: "I just know that every man i kill, the farther away from home i feel.",
+		quotee: "Saving Private Ryan",
+	},
+
+	{
+		number: 108,
+		text: "Worst experiences teach the best lessons.",
+		quotee: "Unknown",
+	},
+
+	{
+		number: 109,
+		text: "Sometimes language has no reasons.",
+		quotee: "Ms. Masda",
+	},
+
+	{
+		number: 110,
+		text: "Sometimes a little discomfort in the beginning can save a whole lot of pain down the road.",
+		quotee: "Chet (Flipped)",
+	},
+
+	{
+		number: 111,
+		text: "Great minds discuss ideas. Average minds discuss events. Small minds discuss people.",
+		quotee: "Elanor Roosevelt",
+	},
+
+	{
+		number: 112,
+		text: "Life is really simple, but we insist on making it complicated.",
+		quotee: "Confucius",
+	},
+
+	{
+		number: 113,
+		text: "Remember Red, hope is a good thing, maybe the best of things, and no good thing ever dies.",
+		quotee: "Andy Dufresne (The Shawshank Redemption)",
+	},
+
+	{
+		number: 114,
+		text: "I guess it comes down a simple choice: Get busy living, or get busy dying.",
+		quotee: " Andy Dufresne (The Shawshank Redemption)",
+	},
+
+	{
+		number: 115,
+		text: "The greatest trick the Devil ever pulled was convincing the world he didn't exist.",
+		quotee: " Roger 'Verbal' Kint (The Usual Suspects)",
+	},
+
+	{
+		number: 116,
+		text: "Try not. Do or do not. There is no try.",
+		quotee: "Yoda (Star Wars)",
+	},
+
+	{
+		number: 117,
+		text: "Those who are happy are not without pain, they just know how not to be controlled by it.",
+		quotee: "Love and Other Drugs",
+	},
+
+	{
+		number: 118,
+		text: "Eat your food as your medicine, otherwise you have to eat medicines as your food.",
+		quotee: "Amatellah",
+	},
+
+	{
+		number: 119,
+		text: "I suppose no man comes home from war. Not really.",
+		quotee: "James Conrad (Kong : Skull Island)",
+	},
+
+	{
+		number: 120,
+		text: "Great man are not born great, they grow great.",
+		quotee: "The Godfather",
+	},
+
+	{
+		number: 121,
+		text: "Happiness is no hesitation. You need to be brave to make a decision when you have the opportunity.",
+		quotee: "Yang Yixue (To My 19 Years Old)",
+	},
+
+	{
+		number: 122,
+		text: "To forgive is not hard. It will only give a little space in your heart.",
+		quotee: "Kim Su-Jin (A Moment To Remember)",
+	},
+
+	{
+		number: 123,
+		text: "If you don't understand the condition, you will always see it in a negative way.",
+		quotee: "Lam (Friendship)",
+	},
+
+	{
+		number: 124,
+		text: "Sometimes people only speak the truth, because they don't care about other people. - Sometimes people lie, because they want other people's approval.",
+		quotee: "Lin Tian Jiao & Gao Xiang (Yesterday Once More)",
+	},
+
+	{
+		number: 125,
+		text: "It's okay to lose people, but never lose yourself.",
+		quotee: "Eminem",
+	},
+
+	{
+		number: 126,
+		text: "With love comes loss, son. It's part of the deal. Sometimes it hurts, but, in the end, it's all worth it. There's no greater gift than love.",
+		quotee: "Stoick (How to Train Your Dragon 3)",
+	},
+
+	{
+		number: 127,
+		text: "Ability may get you to the top, but it takes character to keep you there.",
+		quotee: "Stevie Wonder",
+	},
+
+	{
+		number: 128,
+		text: "The adage is not just literary confectionery. It takes bitter experience to formulate it. ",
+		quotee: "Filosofi Kopi",
+	},
+
+	{
+		number: 129,
+		text: "We cannot equate coffee with sugarcane juice. As perfect as what coffee you make, coffee is still coffee, it has a bitter side that you cannot hide.",
+		quotee: "Filosofi Kopi",
+	},
+
+	{
+		number: 130,
+		text: "When there is no desire, all things are at peace.",
+		quotee: "Laozi",
+	},
+
+	{
+		number: 131,
+		text: "To err is human; to forgive, divine",
+		quotee: "Alexander Pope",
+	},
+
+	{
+		number: 132,
+		text: "Some people are so poor, all they have is money.",
+		quotee: "Bob Marley",
+	},
+
+	{
+		number: 133,
+		text: "Some of us get dipped in flat, some in satin, some in gloss. But every once in a while, you find someone who is iridescent, and when you do, nothing will ever compare.",
+		quotee: "Chet (Flipped by Wendelin van Draanen)",
+	},
+
+];
